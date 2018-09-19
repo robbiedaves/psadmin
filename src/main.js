8f04ae5aebbb7c452a2715640a041dd7713a7c4e
Robin Davies <robbiedaves@hotmail.com>
2018-09-19 20:58:34 +0100

    Add react lifecycle code to add authors

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 $ = jQuery = require('jquery');
 var React = require('react');
 var Home = require('./components/homepage');
+var Authors = require('./components/authors/authorPage');
 var About = require('./components/about/aboutPage');
 var Header = require('./components/common/header');
+
 
 (function(win) {
     "use strict";
@@ -12,6 +14,7 @@ var Header = require('./components/common/header');
 
             switch(this.props.route) {
                 case 'about': Child = About; break;
+                case 'authors': Child = Authors; break;
                 default: Child = Home;
             }
 
