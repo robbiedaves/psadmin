9af1a235d888d1cc3f5c729396ccc3fbcf42064c
Robin Davies <robbiedaves@hotmail.com>
2018-09-27 21:11:28 +0100

    finish off react router section

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ var React = require('react');
 var Router = require('react-router');
 var routes = require('./routes');
 
+//Router.run(routes, Router.HistoryLocation, function(Handler) { // Use this for History Locations URLs instead of Hash based
 Router.run(routes, function(Handler) {
     React.render(<Handler/>, document.getElementById('app'));
 });
