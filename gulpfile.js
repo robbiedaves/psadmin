51d6f1f0bc7c59e0f221630fabf55ad7b0ad3f5a
Robin Davies <robbiedaves@hotmail.com>
2018-09-28 16:22:43 +0100

    Finish react forms

--- gulpfile.js
+++ gulpfile.js
@@ -17,8 +17,9 @@ var config = {
 		js: './src/**/*.js',
 		images: './src/images/*',
 		css: [
-      		'node_modules/bootstrap/dist/css/bootstrap.min.css',
-      		'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+			  'node_modules/bootstrap/dist/css/bootstrap.min.css',
+			  'node_modules/bootstrap/dist/css/bootstrap-theme.min.css',
+			  'node_modules/toastr/toastr.css'
     	],
 		dist: './dist',
 		mainJs: './src/main.js'
